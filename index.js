e34b6fac42ce38de82ae4ae857d50d1a33536343
zach6585 <zmines@wesleyan.edu>
2021-01-05 14:59:53 -0500

    commit tree e5835d416090d2ac2587b1b94fecce6d6861c7e4

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+let driver = {};
+
+function updateDriverWithKeyAndValue(driv, k, v) {
+    let newDriver = { ...driv };
+    newDriver[k] = v;
+    return newDriver
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driv, k, v) {
+    driv[k] = v;
+    return driv
+}
+
+function deleteFromDriverByKey(driv, k) {
+    let newDriver = { ...driv };
+    delete newDriver[k];
+    return newDriver;
+}
+
+function destructivelyDeleteFromDriverByKey(driv, k) {
+    delete driv[k];
+    return driv;
+}
